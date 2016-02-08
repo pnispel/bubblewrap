e2596c047f236eba6c13db975aee78ffcdfed03b
AriaFallah <aria@fallah.us>
2016-02-07 21:40:41 -0500

    :new: first commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# minimal-babel-starter
+# Minimal Babel Starter :rocket:
+
 Get started writing ES6 with an understandable, minimal configuration.
+
+You can find information about this configuration over at this
+[tutorial series](https://github.com/AriaFallah/WebpackTutorial).
+
+In the interest of being minimal this excludes things seen in the tutorial such as the `css-loader`,
+and the `html-webpack-plugin`.
---
+++ webpack.config.dev.js
@@ -0,0 +1,31 @@
+/* eslint no-var: 0 */
+
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  devtool: 'cheap-eval-source-map',
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/dev-server',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    }]
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+}
